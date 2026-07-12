a0942172b322338f37f1103218b9a428a3dfdacc
islammdsohan603 <islammdsohan603@gmail.com>
2026-07-12 15:28:22 +0600

    Products data show

--- next.config.ts
+++ next.config.ts
@@ -4,34 +4,22 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'tse1.mm.bing.net', pathname: '/**' },
-      { protocol: 'https', hostname: 'tse2.mm.bing.net', pathname: '/**' },
-      { protocol: 'https', hostname: 'tse3.mm.bing.net', pathname: '/**' },
-      { protocol: 'https', hostname: 'tse4.mm.bing.net', pathname: '/**' },
-      { protocol: 'https', hostname: '5.imimg.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'andigitallock.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'cdn.shopify.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'gettoeasy.com.bd', pathname: '/**' },
-      { protocol: 'https', hostname: 'i5.walmartimages.com', pathname: '/**' },
       {
         protocol: 'https',
-        hostname: 'image.made-in-china.com',
-        pathname: '/**',
+        hostname: 'images.unsplash.com',
       },
       {
         protocol: 'https',
-        hostname: 'infiniahomeandkitchen.com',
-        pathname: '/**',
+        hostname: 'i.ibb.co',
       },
-      { protocol: 'https', hostname: 'm.media-amazon.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'media.karousell.com', pathname: '/**' },
       {
         protocol: 'https',
-        hostname: 'www.hitachi-homeappliances.com',
-        pathname: '/**',
+        hostname: 'i.ibb.co.com',
       },
-      { protocol: 'https', hostname: 'www.thebrick.com', pathname: '/**' },
+      {
+        protocol: 'https',
+        hostname: 'ibb.co.com',
+      },
     ],
   },
 };
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -10,18 +10,15 @@ interface PageProps {
 }
 
 const ProductsPage = async ({ searchParams }: PageProps) => {
-  // ১. URL থেকে ক্যাটাগরি ও সর্ট ভ্যালু নেওয়া
   const resolvedParams = await searchParams;
   const currentCategory = resolvedParams.category || 'all';
   const currentSort = resolvedParams.sort || '';
 
-  // ২. ব্যাকএন্ড থেকে ফিল্টার করা ডাটা আনা
   const products = await getAllProductsApi(currentCategory, currentSort);
 
   return (
     <div className="min-h-screen bg-[#050508] text-white py-10">
       <section className="w-11/12 max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-4 gap-8">
-        {/* ফিল্টার সেকশন */}
         <div className="col-span-1 space-y-6 bg-[#0d0e12]/50 p-6 border border-gray-900 rounded-2xl h-fit">
           <div>
             <h2 className="text-sm font-semibold text-gray-400 uppercase tracking-wider mb-2">
@@ -37,13 +34,12 @@ const ProductsPage = async ({ searchParams }: PageProps) => {
           </div>
         </div>
 
-        {/* প্রোডাক্ট লিস্ট গ্রিড */}
         <div className="col-span-3">
           <h1 className="text-2xl font-bold mb-6 text-transparent bg-clip-text bg-linear-to-b from-white to-gray-400">
             Curated Digital Collection ({products.length})
           </h1>
 
-          {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {products.length > 0 ? (
               products.map(product => (
                 <ProdcutCard key={product._id} product={product} />
@@ -53,7 +49,7 @@ const ProductsPage = async ({ searchParams }: PageProps) => {
                 No products found in this category.
               </p>
             )}
-          </div> */}
+          </div>
         </div>
       </section>
     </div>
