58f55a3c879ec62faeb5856442caf06c25b50947
islammdsohan603 <islammdsohan603@gmail.com>
2026-07-12 21:13:54 +0600

    GitHub Buttons funcnality add

--- src/components/socalButton/GitHub.tsx
+++ src/components/socalButton/GitHub.tsx
@@ -1,10 +1,21 @@
+import { authClient } from '@/lib/auth-client';
 import React from 'react';
 import { FaGithub } from 'react-icons/fa6';
 
 const GitHub = () => {
+  const handleGitHubSignin = async () => {
+    await authClient.signIn.social({
+      provider: 'github',
+    });
+  };
+
   return (
     <div>
-      <button className="flex w-full items-center justify-center gap-2 py-3 bg-[#050508]/80 border border-gray-900 hover:border-gray-700 rounded-xl text-sm font-medium text-gray-300 hover:text-white transition-all duration-300 cursor-pointer active:scale-95">
+      <button
+        type="submit"
+        onClick={handleGitHubSignin}
+        className="flex w-full items-center justify-center gap-2 py-3 bg-[#050508]/80 border border-gray-900 hover:border-gray-700 rounded-xl text-sm font-medium text-gray-300 hover:text-white transition-all duration-300 cursor-pointer active:scale-95"
+      >
         <FaGithub className="w-4 h-4 text-gray-400 group-hover:text-white" />
         <span className="text-xs">GitHub</span>
       </button>
