a09a098835b1e4ef788e81f240f76a9eea9cdacf
islammdsohan603 <islammdsohan603@gmail.com>
2026-07-16 09:32:27 +0600

    products data api call updata

--- src/db/productsdataapi.ts
+++ src/db/productsdataapi.ts
@@ -1,4 +1,4 @@
-const SERVER_URL = (process.env.NEXT_PUBLIC_SERVER_URL || '').replace(
+const SERVER_URL = (process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:5000').replace(
   /\/+$/,
   '',
 );
